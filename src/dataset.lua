d82b53298aca7f0045ba3ff80b4bcb2f99e43712
Hujie Wang <wanghujie@gmail.com>
2015-03-30 23:00:18 -0400

    Use torch.class for DataSet class instead of using middleclass

--- src/dataset.lua
+++ src/dataset.lua
@@ -2,36 +2,34 @@
 DataSet Class
 --]]
 
-local class = require 'middleclass'
-
-local DataSet = class('DataSet')
+do
+  local DataSet = torch.class('DataSet')
 
 --[[
 input: Tensor
 target: Tensor
 --]]
-function DataSet:initialize(input,target,vocab,opt)
-  assert(input:size(1)==target:size(1))
-  self.opt=opt
-  self.input=input
-  self.target=target
-  self.vocab=vocab
-  self.data_size=input:size(1)
-  self.shuffle = torch.randperm(self.data_size)
-end
-
-function getBatch(batch)
-  local s=batch*(batch_size-1)+1
-  local e=math.min(batch*self.opt.batch_size,self.data_size)
-  local input_batch={}
-  local target_batch={}
-  for i=s,e do
-    table.insert(input_batch,self.input[shuffle[i]])
-    table.insert(target_batch,self.target[shuffle[i]])
+  function DataSet:__init(input,target)
+    assert(input:size(1)==target:size(1))
+    self.input=input
+    self.target=target
+    self.data_size=input:size(1)
+    self.shuffle = torch.randperm(self.data_size)
   end
-  return input_batch,target.batch
-end
 
-function shuffle()
-  self.shuffle = torch.randperm(self.data_size)
+  function getBatch(batch, batch_size)
+    local s=batch*(batch_size-1)+1
+    local e=math.min(batch*self.opt.batch_size,self.data_size)
+    local input_batch={}
+    local target_batch={}
+    for i=s,e do
+      table.insert(input_batch,self.input[shuffle[i]])
+      table.insert(target_batch,self.target[shuffle[i]])
+    end
+    return input_batch,target_batch
+  end
+
+  function shuffle()
+    self.shuffle = torch.randperm(self.data_size)
+  end
 end
