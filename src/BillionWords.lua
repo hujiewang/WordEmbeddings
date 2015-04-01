d995bc955420f62eb68b571ca0e96cfbacd3ccda
Hujie Wang <wanghujie@gmail.com>
2015-04-01 19:45:20 -0400

    Fixed minor bugs

--- src/BillionWords.lua
+++ src/BillionWords.lua
@@ -7,7 +7,7 @@ do
   local BillionWords = torch.class('BillionWords')
   
   --[[
-  opt={
+  b_opt={
     train_tiny
     train_small
     train_full
@@ -20,8 +20,9 @@ do
   }
   --]]
   
-  function BillionWords:__init(opt)
+  function BillionWords:__init(b_opt, opt)
     self.opt = opt
+    self.b_opt = b_opt
   end
 
   function BillionWords:load(file_path)
@@ -34,31 +35,31 @@ do
     data_size = data_size or "tiny"
     
     if data_size == "tiny" then
-      self.train_data = self:load(self.opt.train_tiny)
+      self.train_data = self:load(self.b_opt.train_tiny)
     elseif data_size == "small" then
-      self.train_data = self:load(self.opt.train_small)
+      self.train_data = self:load(self.b_opt.train_small)
     elseif data_size == "full" then
-      self.train_data = self:load(self.opt.train_full)
+      self.train_data = self:load(self.b_opt.train_full)
     end
-    self.valid = self:load(self.opt.valid_data)
-    self.test = self:load(self.opt.test_data)
-    self.word_map = self:load(self.opt.word_map)
-    self.word_tree = self:load(self.opt.word_tree)
+    self.valid = self:load(self.b_opt.valid_data)
+    self.test = self:load(self.b_opt.test_data)
+    self.word_map = self:load(self.b_opt.word_map)
+    self.word_tree = self:load(self.b_opt.word_tree)
     
     -- Loads data
     local input_list = {}
     local target_list = {}
     local cur_sentence = {}
     
-    assert(self.train_data[self.train_data:size(1)][2] == self.opt.sentence_end_id, "train_data should be terminated with sentence_end_id")
+    assert(self.train_data[self.train_data:size(1)][2] == self.b_opt.sentence_end_id, "train_data should be terminated with sentence_end_id")
     
-    --for i = 1, self.train_data:size(1) - 1 do
-    for i = 1, 6 do
+    for i = 1, self.train_data:size(1) - 1 do
+    --for i = 1, 6 do
       table.insert(cur_sentence, self.train_data[i][2])
       
       local input = {}
-      for j = 1, self.opt.context_size - #cur_sentence do
-        table.insert(input,self.opt.sentence_start_id)
+      for j = 1, self.b_opt.context_size - #cur_sentence do
+        table.insert(input,self.b_opt.sentence_start_id)
       end
       
       local target = self.train_data[i+1][2]
@@ -70,23 +71,25 @@ do
       table.insert(input_list,input)
       table.insert(target_list,target)
       
-      if #cur_sentence == self.opt.context_size then
+      if #cur_sentence == self.b_opt.context_size then
         table.remove(cur_sentence,1)
       end
-      if self.train_data[i][2] == self.opt.sentence_end_id then
+      if self.train_data[i][2] == self.b_opt.sentence_end_id then
         while #cur_sentence ~= 0 do
           table.remove(cur_sentence,1)
         end
       end
     end
-    local dataset=DataSet(torch.IntTensor(input_list),torch.IntTensor(target_list))
+    local dataset=DataSet(torch.IntTensor(input_list),torch.IntTensor(target_list),opt)
     return dataset
   end
   
   
 end
--- Tests
-billionwords_opt = {
+
+--[[
+Tests
+billionwords_b_opt = {
   word_map = "../data/billionwords/word_map.th7",
   test_data = "../data/billionwords/test_data.th7",
   valid_data = "../data/billionwords/valid_data.th7",
@@ -100,6 +103,8 @@ billionwords_opt = {
   sentence_unknown_id = 793469,
   root_id = 880542
 }
-test = BillionWords(billionwords_opt)
+test = BillionWords(billionwords_b_opt)
 dataset = test:loadData("tiny")
-print(dataset.input)
+print(#test.word_map)
+--]]
+
