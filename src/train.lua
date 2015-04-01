9c3180fc5ae3017915c45a8719e797470b8213ab
Hujie Wang <wanghujie@gmail.com>
2015-04-01 19:47:37 -0400

    Finished init tests, removed confusion matrix(too large), few bugs fixed

--- src/train.lua
+++ src/train.lua
@@ -1,24 +1,12 @@
 require 'torch'
 require 'optim'
 require 'xlua'
-
+require 'cutorch'
+require 'nn'
+--require('mobdebug').start()
 
 function train(model,criterion,dataset,opt)
   trainLogger = optim.Logger(paths.concat(opt.save,'train.log'))
-  classes={}
-  for i=1,opt.vocab.size do
-    table.insert(classes,i)
-  end
-  confusion = optim.ConfusionMatrix(classes)
-
-  if model then
-    parameters,gradParameters=model:getParameters()
-  end
-
-  if opt.type == 'cuda' then
-    model:cuda()
-    criterion:cuda()
-  end
 
   if opt.optimization == 'CG' then
     optimState = {
@@ -42,57 +30,101 @@ function train(model,criterion,dataset,opt)
       learningRateDecay = 1e-7
     }
     optimMethod = optim.sgd
+  else
+    error('unknown optimization method')
+  end
 
-    for epoch = 1,opt.max_epochs do
-      local time = sys.clock()
-      for batch=1,opt.batch_size do
+  for epoch = 1,opt.max_epochs do
+    local time = sys.clock()
+    --for batch=1,opt.batch_size do
+    for batch=1,1 do
+      -- displays progress
+      xlua.progress(batch,opt.batch_size)
 
-        -- displays progress
-        xlua.progress(batch,opt.batch_size)
+      local inputs,targets = dataset:getBatch(batch)
 
-        local input,target = dataset:getBatch(batch)
-
-        local function feval(x)
-          -- get new parameters
-          if x~=parameters then
-            parameters:copy(x)
-          end
-
-          -- reset gradients
-          gradParameters:zero()
-
-          local cost=0
-
-          for i=1,#input do
-            local output = model:forward(input[i])
-            local err = criterion:forward(output,target[i])
-            cost = cost + err
-
-            local df_do=criterion:backward(output,target[i])
-              model:backward(inputs[i],df_do)
-
-              confusion:add(output,targets[i])
-          end
-
-          gradParameters:div(#input)
-          cost = cost/#inputs
-          return cost,gradParameters
+      local function feval(x)
+        -- get new parameters
+        if x~=parameters then
+          parameters:copy(x)
         end
 
-          optimMethod(feval,parameters,opt.optimState)
+        -- reset gradients
+        gradParameters:zero()
+
+        local cost=0
+
+        for i=1,#inputs do     
+
+          local output = model:forward(inputs[i])
+          local err = criterion:forward(output,targets[i])
+          cost = cost + err
+
+          local df_do=criterion:backward(output,targets[i])
+          model:backward(inputs[i],df_do)
+
+          --confusion:add(output,targets[i])
         end
-        time = sys.clock() - time
-        time = time / opt.data_size
-        print("==> time to learn 1 sample = " .. (time*1000) .. "ms\n")
-        print(confusion)
-        train_logger:add{["% mean class accuracy "]=confusion.totalValid*100}
-        -- next epoch
-        confusion:zero()
-        dataset:shuffle()
+
+        gradParameters:div(#input)
+        cost = cost/#inputs
+        return cost,gradParameters
       end
-      local model_file=paths.concat(opt.save,"model.net")
-      os.execute('mkdir -p' .. sys.dirname(model_file))
-      torch.save(model_file,model)
-      print("==> Saving model completed!\n")
-
+      
+      parameters,gradParameters = model:getParameters();
+      --optimMethod(feval,parameters,opt.optimState)
+      optim.sgd(feval,parameters,opt.optimState)
     end
+    time = sys.clock() - time
+    time = time / dataset:size()
+    print("==> time to learn 1 sample = " .. (time*1000) .. "ms\n")
+    print("==> mean class accuracy = " .. (confusion.totalValid*100) .."%\n")
+    --print(confusion)
+    trainLogger:add{["% mean class accuracy "]=confusion.totalValid*100}
+    -- next epoch
+    dataset:shuffle()
+  end
+    local model_file=paths.concat(opt.save,"model.net")
+    os.execute('mkdir -p' .. sys.dirname(model_file))
+    torch.save(model_file,model)
+    print("==> Saving model completed!\n")
+
+end
+
+--[[
+  -- test
+  opt={
+    -- Training hyperparameters
+    type = 'int',
+    optimization = 'SGD',
+    learning_rate = 1e-3,
+    weight_decay = 0.1,
+    momentum = 0.9,
+    batch_size = 1,
+    loss = 'nll ',
+    max_epochs=1,
+
+    -- Data parameters
+    word_embedding_size = 50,
+    context_size = 5,
+    vocab_size = 100,
+    
+    -- Model parameters
+    hidden_layer_size = 10,
+    output_layer_size = 100,
+
+    -- Logger
+    save="../log/"
+  }
+
+  model=nn.Sequential()
+  model:add(nn.LookupTable(opt.vocab_size,opt.word_embedding_size))
+  model:add(nn.Reshape(opt.context_size*opt.word_embedding_size))
+  model:add(nn.Linear(opt.context_size*opt.word_embedding_size,opt.hidden_layer_size))
+  model:add(nn.Tanh())
+  model:add(nn.Linear(opt.hidden_layer_size,opt.output_layer_size))
+  model:add(nn.LogSoftMax())
+
+  criterion = nn.ClassNLLCriterion()
+  train(model,criterion,nil,opt)
+  --]]
