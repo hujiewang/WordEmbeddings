dd91235aa2dc78c8c3bd082d0ba5c98dd046e30b
Hujie Wang <wanghujie@gmail.com>
2015-04-08 02:23:51 -0400

    Separated data generation from loadDada(), added a size limitation for generated data

--- src/BillionWords.lua
+++ src/BillionWords.lua
@@ -3,24 +3,10 @@
 
 do
   local BillionWords = torch.class('BillionWords')
+
   
-  --[[
-  b_opt={
-    train_tiny
-    train_small
-    train_full
-    valid_data
-    test_data
-    word_map
-    context_size
-    sentence_start_id
-    sentence_end_id
-  }
-  --]]
-  
-  function BillionWords:__init(b_opt, opt)
+  function BillionWords:__init(opt)
     self.opt = opt
-    self.b_opt = b_opt
   end
 
   function BillionWords:load(file_path)
@@ -28,50 +14,42 @@ do
     collectgarbage()
     return data
   end
-
-  function BillionWords:loadData(data_size)
-    data_size = data_size or "tiny"
-    
-    if data_size == "tiny" then
-      self.train_data = self:load(self.b_opt.train_tiny)
-    elseif data_size == "small" then
-      self.train_data = self:load(self.b_opt.train_small)
-    elseif data_size == "full" then
-      self.train_data = self:load(self.b_opt.train_full)
-    end
-    self.valid = self:load(self.b_opt.valid_data)
-    self.test = self:load(self.b_opt.test_data)
-    self.word_map = self:load(self.b_opt.word_map)
-    self.word_tree = self:load(self.b_opt.word_tree)
-    
+  
+  --[[
+    input: a raw dataset
+    output: a dataset consists of 'context_size-gram'
+  --]]
+  function BillionWords:generateData(data, max_size)
     -- Loads data
     local input_list = {}
     local target_list = {}
     local cur_sentence = {}
     
-    assert(self.train_data[self.train_data:size(1)][2] == self.b_opt.sentence_end_id, "train_data should be terminated with sentence_end_id")
+    assert(data[data:size(1)][2] == self.opt.sentence_end_id, "data should be terminated with sentence_end_id")
     
-    for i = 1, self.train_data:size(1) - 1 do
-      table.insert(cur_sentence, self.train_data[i][2])
+    for i = 1, data:size(1) - 1 do
+      table.insert(cur_sentence, data[i][2])
       
       local input = {}
-      for j = 1, self.b_opt.context_size - #cur_sentence do
-        table.insert(input,self.b_opt.sentence_start_id)
+      for j = 1, self.opt.context_size - #cur_sentence do
+        table.insert(input,self.opt.sentence_start_id)
       end
       
-      local target = self.train_data[i+1][2]
+      local target = data[i+1][2]
       
       for j = 1, #cur_sentence do
         table.insert(input, cur_sentence[j])
       end
-      assert(#input == self.b_opt.context_size)
+      assert(#input == self.opt.context_size)
       table.insert(input_list,input)
       table.insert(target_list,target)
-      
-      if #cur_sentence == self.b_opt.context_size then
+      if #input_list > max_size then
+        break
+      end
+      if #cur_sentence == self.opt.context_size then
         table.remove(cur_sentence,1)
       end
-      if self.train_data[i][2] == self.b_opt.sentence_end_id then
+      if data[i][2] == self.opt.sentence_end_id then
         while #cur_sentence ~= 0 do
           table.remove(cur_sentence,1)
         end
@@ -81,6 +59,34 @@ do
     return dataset
   end
   
+  function BillionWords:loadData(data_size)
+    data_size = data_size or "tiny"
+    
+    if data_size == "tiny" then
+      self.train_data = self:load(self.opt.train_tiny)
+    elseif data_size == "small" then
+      self.train_data = self:load(self.opt.train_small)
+    elseif data_size == "full" then
+      self.train_data = self:load(self.opt.train_full)
+    end
+    self.valid_data = self:load(self.opt.valid_data)
+    self.test_data = self:load(self.opt.test_data)
+    self.word_map = self:load(self.opt.word_map)
+    self.word_tree = self:load(self.opt.word_tree)
+    
+    -- Generates dataset for training, validation, and testing
+    self.train_dataset=self:generateData(self.train_data,100000)
+    self.valid_dataset=self:generateData(self.valid_data,100000)
+    self.test_dataset=self:generateData(self.test_data,100000)
+    return self.train_dataset,self.valid_dataset,self.test_dataset
+  end
+  
+  function BillionWords:getSoftMaxTreeLayer()
+      self.hierarchy = self:load(self.opt.word_tree)
+     
+      local smt = nn.SoftMaxTree(self.opt.word_embedding_size, self.hierarchy, self.opt.root_id)
+      return smt
+  end
   
 end
 
