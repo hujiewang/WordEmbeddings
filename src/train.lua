9492001e9f302a1ea1573fec2456a993c415662a
Hujie Wang <wanghujie@gmail.com>
2015-04-28 22:09:12 -0400

    Using SGD class to train

--- src/train.lua
+++ src/train.lua
@@ -10,44 +10,17 @@ function train(model,criterion,dataset,opt)
   local last_avg_cost = math.huge
   local avg_cost
   
-  if opt.optimization == 'CG' then
+  if opt.optimization == 'SGD' then
     optimState = {
-      maxIter = opt.maxIter
-    }
-    optimMethod = optim.cg
-
-  elseif opt.optimization == 'LBFGS' then
-    optimState = {
-      learningRate = opt.learningRate,
-      maxIter = opt.maxIter,
-      nCorrection = 10
-    }
-    optimMethod = optim.lbfgs
-
-  elseif opt.optimization == 'SGD' then
-    optimState = {
-      learningRate = opt.learningRate,
-      weightDecay = opt.weightDecay,
+      learning_rate = opt.learning_rate,
+      weight_decay= opt.weight_decay,
       momentum = opt.momentum,
-      learningRateDecay = 1e-7
+      learning_rate_decay = 1e-5
     }
-    optimMethod = optim.sgd
+    sgd = SGD(model,criterion,optimState)
   else
     error('unknown optimization method')
   end
-  --[[
-  trainer = nn.StochasticGradient(model, criterion);
-  trainer.learningRate = opt.learning_rate;
-  trainer.maxIteration = 1;
-  --]]
-  para={}
-  para.mlp={}
-  para.smt={}
-  para.mlp.parameters,para.mlp.gradParameters = model.mlp:parameters()
-  para.smt.parameters,para.smt.gradParameters = model.smt:parameters()
-
-  
-  parameters,gradParameters = flattenParameters({para.mlp.parameters,para.smt.parameters},{para.mlp.gradParameters,para.smt.gradParameters})
  
   for epoch = 1,opt.max_epochs do
     local time = sys.clock()
@@ -58,43 +31,7 @@ function train(model,criterion,dataset,opt)
       xlua.progress(batch,math.ceil(dataset:size()/opt.batch_size))
       
       local ds,inputs,targets = dataset:getBatch(batch)
-      
-      local function feval(x)
-        
-        --[[
-        -- get new parameters
-        if x~=parameters then
-          parameters:copy(x)
-        end
-        --]]
-        
-        
-        -- reset gradients
-        gradParameters:zero()
-        
-        
-        local mlp_output = model.mlp:forward(inputs)
-        local stm_output = model.smt:forward({mlp_output,targets})
-        
-        -- Note that TreeNLLCriterion is CPU-based, we need to copy data from GPU to CPU
-        stm_output = stm_output:double()
-        local err = criterion:forward(stm_output,targets)
-        
-        local df_do = criterion:backward(stm_output,targets)
-          -- Then we need to copy data from CPU to GPU
-        df_do = df_do:cuda()
-        
-        local smt_grad_input = model.smt:backward({mlp_output,targets},df_do)
-        
-        -- SoftMaxTree backward produces {gradInput, gradTarget}, we only need gradInput
-        smt_grad_input = smt_grad_input[1]
-        model.mlp:backward(inputs,smt_grad_input)
-        
-        cost=cost+err
-        return err,gradParameters
-      end
-      optimMethod(feval,parameters,opt.optimState)
-
+      cost = cost + sgd:optimize(inputs,targets)
     end
     time = sys.clock() - time
     avg_cost = (cost/math.ceil(dataset:size()/opt.batch_size))
@@ -118,7 +55,7 @@ function train(model,criterion,dataset,opt)
     last_avg_cost = avg_cost
     local model_file=paths.concat(opt.save,"model.net")
     --os.execute('mkdir -p' .. sys.dirname(model_file))
-    torch.save(model_file,model)
+    --torch.save(model_file,model)
     print("==> Saving model completed!\n")
     
     -- next epoch
