d2056f1b911c3bf0c2eec7dbf1922ac974f3377a
Hujie Wang <wanghujie@gmail.com>
2015-04-28 22:07:39 -0400

    Use ParallelTable to simplify training process

--- src/model.lua
+++ src/model.lua
@@ -1,25 +1,30 @@
 
+function makeParallel(layer)
+  pt = nn.ParallelTable()
+  pt:add(layer)
+  pt:add(nn.Identity())
+  return pt
+end
 
 function getModel(opt,billionwords)
   
   model = {}
-  model.mlp = nn.Sequential()
-  model.mlp:add(nn.LookupTable(opt.vocab_size,opt.word_embedding_size))
-  model.mlp:add(nn.Reshape(opt.context_size*opt.word_embedding_size))
-  model.mlp:add(nn.Dropout())
-  model.mlp:add(nn.Linear(opt.context_size*opt.word_embedding_size,opt.hidden_layer_size))
-  model.mlp:add(nn.Tanh())
-  model.mlp:add(nn.Dropout())
-  model.mlp:add(nn.Linear(opt.hidden_layer_size,opt.output_layer_size))
-  model.mlp:add(nn.Tanh())
-  model.smt = billionwords:getSoftMaxTreeLayer()
+  model = nn.Sequential()
+  model:add(makeParallel(nn.LookupTable(opt.vocab_size,opt.word_embedding_size)))
+  model:add(makeParallel(nn.Reshape(opt.context_size*opt.word_embedding_size)))
+  model:add(makeParallel(nn.Dropout()))
+  model:add(makeParallel(nn.Linear(opt.context_size*opt.word_embedding_size,opt.hidden_layer_size)))
+  model:add(makeParallel(nn.Tanh()))
+  model:add(makeParallel(nn.Dropout()))
+  model:add(makeParallel(nn.Linear(opt.hidden_layer_size,opt.output_layer_size)))
+  model:add(makeParallel(nn.Tanh()))
+  model:add(billionwords:getSoftMaxTreeLayer())
 
   criterion = nn.TreeNLLCriterion()
   
   if opt.type == 'cuda' then
       -- Moves to CUDA
-    model.mlp:cuda()
-    model.smt:cuda()
+    model:cuda()
   end
   
   return model, criterion
